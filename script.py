23fb7a1c66acf22fb1d256fb5367eb2ee1509b58
duskvirkus <duskvirkus@protonmail.com>
2022-01-25 03:51:12 -0700

    some fixes

--- script.py
+++ script.py
@@ -4,12 +4,27 @@ import click
 import os
 
 @click.command()
-@click.option('-i', '--input_path', type=click.Path(exists=True), required=True)
-@click.option('-o', '--output_path', type=click.Path(exists=False), required=True)
+@click.option(
+    '-i',
+    '--input_path',
+    type=click.Path(exists=True),
+    required=True,
+    help='Path to folder of images.',
+)
+@click.option(
+    '-o',
+    '--output_path',
+    type=click.Path(exists=False),
+    required=True,
+    help='Path of resulting image. Must end in .png. Necessary folder created automatically.',
+)
 def long_exposure(
     input_path,
     output_path,
 ):
+
+    if output_path.split('.')[-1] != 'png':
+        raise Exception(f'output_path must end in .png! path provided: {output_path}')
 
     images = []
 
@@ -26,13 +41,15 @@ def long_exposure(
     out_image = np.zeros((shape[0], shape[1], 3), np.float32)
 
     for i in images:
-        out_image += i
+        if i.shape != shape:
+            print(f'WARNING: {i.shape} does not equal first image shape. Skipping')
+        else:
+            out_image += i
 
-    out_image /= len(images)
+    out_image /= len(images) / 15
+    out_image = np.clip(out_image, 0, 255)
     out_image = out_image.astype(np.uint8)
-
-    assert(output_path.split('.')[-1] == 'png')
-
+        
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     cv2.imwrite(output_path, out_image, [cv2.IMWRITE_PNG_COMPRESSION, 0])
 
